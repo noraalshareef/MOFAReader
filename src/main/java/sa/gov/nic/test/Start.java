69cbf30abe9f82b5fe840314d7bc2689f20a38fc
alanoudfq <78420554+alanoudfq@users.noreply.github.com>
2021-02-03 11:10:36 +0300

    second commit

--- src/main/java/sa/gov/nic/test/Start.java
+++ src/main/java/sa/gov/nic/test/Start.java
@@ -17,6 +17,7 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("hello world ");
                 System.out.println("hi anoud ");
+                int x;
 
     }
     
